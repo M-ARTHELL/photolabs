a7099bdbceaff221cc2635c51828a42a1a90370a
M-ARTHELL <makoarthell@proton.me>
2024-01-29 16:54:05 -0400

    add style to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,7 +6,7 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   
   return (
-    <div key={props.sampleData.id}>
+    <div className="photo-list__item">
       <div><img src={props.sampleData.imageSource} /></div>
       <div><img src={props.sampleData.profile} />{props.sampleData.username}</div>
       <div>{props.sampleData.location.city}, {props.sampleData.location.country}</div>
