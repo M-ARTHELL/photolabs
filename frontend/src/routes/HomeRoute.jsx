2fe99fdc1f6e0bf2ea3fb410c3b79c82b05ee2e5
M-ARTHELL <makoarthell@proton.me>
2024-02-04 18:18:28 -0400

    remove unused imports

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import PhotoListItem from '../components/PhotoListItem';
-import PhotoFavButton from '../components/PhotoFavButton';
 import PhotoList from 'components/PhotoList';
 import TopNavigation from 'components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
