75119b300a6239680b91ea4f4be5e98f3c6ca71d
M-ARTHELL <makoarthell@proton.me>
2024-02-09 18:39:02 -0400

    remove unneeded props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,7 +3,7 @@ import TopicList from 'components/TopicList';
 import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = ({toggleFavs, favorites, setFavorites}) => {
+const TopNavigation = ({favorites}) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
