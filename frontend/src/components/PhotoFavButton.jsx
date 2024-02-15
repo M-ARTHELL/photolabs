eee85e677f3981889364b05909de333769ed9724
M-ARTHELL <makoarthell@proton.me>
2024-02-14 23:47:52 -0400

    rename props

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,18 +1,16 @@
 import React, { useCallback, useState } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 
-
-function PhotoFavButton({id, favHandler}) {
+function PhotoFavButton({id, favorites, toggleFavs}) {
 
   return (
     <div
       className="photo-list__fav-icon"
-      onClick={() => favHandler.toggleFavs(id)}>
+      onClick={() => toggleFavs(id)}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={favHandler.favorites.includes(id)} />
+        <FavIcon selected={favorites.includes(id)} />
       </div>
     </div>
   );
